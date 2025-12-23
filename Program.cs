103d8f33a7988b6a2a68550601da023ee6ae3308
Марічка <marichkamelnyk07@gmail.com>
2025-12-23 20:40:42 +0200

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+int heroHealth = 100;
+int monsterHealth = 100;
+
+while (true)
+{
+    int heroDamage = Attack();
+    monsterHealth -= heroDamage;
+    if (monsterHealth <= 0)
+    {
+        Console.WriteLine($"Герой наносить {heroDamage} урону та перемагає!");
+        break;
+    }
+    else
+    {
+        Console.WriteLine($"Герой вдарив на {heroDamage} урону. У монстра залишилось {monsterHealth} HP\n");
+    }
+    int monsterDamage = Attack();
+    heroHealth -= monsterDamage;
+    if (heroHealth <= 0)
+    {
+        Console.WriteLine($"Монстр наносить {monsterDamage} урону та перемагає!");
+        break;
+    }
+    else
+    {
+        Console.WriteLine($"Монстр вдарив на {monsterDamage} урону. У героя залишилось {heroHealth} HP\n");
+    }
+}
+
+static int Attack()
+{
+    Random random = new Random();
+    return random.Next(5, 21);
+}
